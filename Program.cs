297701f770056716ff464c9a00035ee3ce413468
teresa50530 <59741038+teresa50530@users.noreply.github.com>
2020-02-14 17:17:49 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 閏年
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.Write("請輸入一個民國年份");
+            string i = Console.ReadLine();
+            int value = int.Parse(i)+1911;
+          
+            if (DateTime.IsLeapYear(value) == true)
+            {
+                Console.WriteLine(value+"為閏年");
+            }
+            else
+            {
+                Console.WriteLine(value+"為平年");
+            }
+            Console.ReadLine();
+        }
+    }
+}
